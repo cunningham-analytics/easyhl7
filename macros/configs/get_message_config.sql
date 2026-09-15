44984a9862237ba7c49bd718c55a8cf1e812ff4d
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-15 15:50:39 -0400

    oru and orm 2.3 configs

--- macros/configs/get_message_config.sql
+++ macros/configs/get_message_config.sql
@@ -24,6 +24,12 @@
     {% elif version == '2.3.1' and message_type == 'ORM_O01' %}
         {{ return(easyhl7.config_v2_3_1_ORM_O01()) }}
 
+    {% elif version == '2.3' and message_type == 'ORU_R01' %}
+    {{ return(easyhl7.config_v2_3_ORU_R01()) }}
+
+    {% elif version == '2.3' and message_type == 'ORM_O01' %}
+        {{ return(easyhl7.config_v2_3_ORM_O01()) }}
+
     {% else %}
         {{ exceptions.raise_compiler_error(
             "EasyHL7 does not support version="
