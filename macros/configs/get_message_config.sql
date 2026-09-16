58bafcb024bc261d7c6aff62612a096f0c6cc611
cunningham-analytics <cunninghamc.analytics@gmail.com>
2026-09-16 13:01:41 -0400

    adding 2.3.1 oru config

--- macros/configs/get_message_config.sql
+++ macros/configs/get_message_config.sql
@@ -29,6 +29,9 @@
 
     {% elif version == '2.3' and message_type == 'ORM_O01' %}
         {{ return(easyhl7.config_v2_3_ORM_O01()) }}
+    
+    {% elif version == '2.3.1' and message_type == 'ORU_R01' %}
+    {{ return(easyhl7.config_v2_3_1_ORU_R01()) }}
 
     {% else %}
         {{ exceptions.raise_compiler_error(
